4b3d1e35ee5ea50c8751721e4fa2661d3a9f31bd
edithcarollaine <edith.carollaine12@gmail.com>
2024-01-09 14:40:09 -0300

    update .gitignore

--- API DataBase/utils.py
+++ API DataBase/utils.py
@@ -24,27 +24,27 @@ mostrar = session.query(Exercicio).order_by()
 for result in mostrar:
     print(result)
 
-print('\nUPDATE DE DADOS EM USER')
+print('\nUPDATE EM USER')
 alterar = session.execute(update(User).where(User.id.in_([3])).values(idade=30))
 busca_dois = session.query(User).filter_by(id=3)
 for result in busca_dois:
     print(result)
 
-print('\nINSERT DE DADOS EM USER')
+print('\nINSERT EM USER')
 inserir_um = session.execute(insert(User).values(nome_pessoa='joao', idade=18))
 inserir_dois = session.execute(insert(User).values(nome_pessoa='felipe', idade=50))
 print('Valores inseridos com sucesso!')
 
-print('\nMOSTRANDO VALORES ATUALIZADOS APÓS INSERÇÃO')
+print('\nMOSTRANDO VALORES ATUALIZADOS')
 mostrar = session.query(User).order_by()
 for result in mostrar:
     print(result)
 
-print('\nDELETE DE DADOS EM USER')
+print('\nDELETE EM USER')
 delete = session.execute(delete(User).where(User.id.in_([5, 6])))
 print('Valores deletados com sucesso!')
 
-print('\nMOSTRANDO VALORES ATUALIZADOS APÓS DELETE')
+print('\nMOSTRANDO VALORES ATUALIZADOS')
 mostrar = session.query(User).order_by()
 for result in mostrar:
     print(result)
